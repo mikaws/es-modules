04ab93b91cfa33975a4a7cab3c663e4fa94de46e
MichaelHenrique <michaelhenriqued@gmail.com>
2021-05-12 22:34:14 -0300

    Currency Format with ES Modules

---
+++ esmodule/node/index.mjs
@@ -0,0 +1,12 @@
+//import CurrencyManager from "../shared/currencyManager.mjs"
+import CurrencyManager from "shared/currencyManager"
+
+function updateOutput(value){
+  console.log(value)
+}
+
+const inputMonitor = (value) => updateOutput(CurrencyManager.format(value))
+
+const input = process.openStdin()
+console.log('typing...')
+input.addListener("data", inputMonitor)
